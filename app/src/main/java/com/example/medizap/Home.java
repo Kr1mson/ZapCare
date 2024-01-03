f96acf660cc3dcdfe70bf834bc00432c8fd06aaa
NotShivain <shivain65@gmail.com>
2024-01-03 20:59:40 +0530

    Firebase Login Signup

---
+++ app/src/main/java/com/example/medizap/Home.java
@@ -0,0 +1,14 @@
+package com.example.medizap;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.home_main);
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
