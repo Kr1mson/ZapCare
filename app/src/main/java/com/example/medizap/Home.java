50cdc5e5745add30335184f222fc46e43a9ae9b5
ibekshitiz <ibekshitiz@gmail.com>
2024-01-08 00:41:31 +0530

    Kshitiz_steps

--- app/src/main/java/com/example/medizap/Home.java
+++ app/src/main/java/com/example/medizap/Home.java
@@ -2,9 +2,14 @@ package com.example.medizap;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,7 +26,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 
-public class Home extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, NavigationView.OnNavigationItemSelectedListener  {
+public class Home extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, NavigationView.OnNavigationItemSelectedListener, SensorEventListener {
     DrawerLayout drawerLayout;
     BottomNavigationView bottomNavigationView;
     Map firstFragment = new Map();
@@ -31,7 +36,11 @@ public class Home extends AppCompatActivity implements BottomNavigationView.OnNa
     Reports fifthFragment = new Reports();
     TextView name,email;
     Button test;
-
+    TextView textViewStepCounter;
+    protected SensorManager sensorManager;
+    Sensor mStepCounter;
+    boolean isCounterSensorPresent;
+    int stepCount =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,6 +74,18 @@ public class Home extends AppCompatActivity implements BottomNavigationView.OnNa
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.Tracker);
 
+        textViewStepCounter = findViewById(R.id.textViewStepCounter);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null){
+            mStepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            isCounterSensorPresent = true;
+        }
+        else{
+            textViewStepCounter.setText("Counter Sensor is Not Present");
+            isCounterSensorPresent = false;
+        }
 
     }
 
@@ -116,5 +137,33 @@ public class Home extends AppCompatActivity implements BottomNavigationView.OnNa
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.sensor == mStepCounter){
+            stepCount = (int) sensorEvent.values[0];
+            textViewStepCounter.setText(String.valueOf(stepCount));
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null)
+            sensorManager.registerListener(this, mStepCounter, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null)
+            sensorManager.unregisterListener(this,mStepCounter);
     }
 }
