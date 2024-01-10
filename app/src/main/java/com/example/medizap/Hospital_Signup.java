c03c2b7ada98a25db817f61ea58d02d4ac697b39
Parth Passi <parthpassi@gmail.com>
2024-01-10 14:33:15 +0530

    Commit_10/01/2024/1

---
+++ app/src/main/java/com/example/medizap/Hospital_Signup.java
@@ -0,0 +1,14 @@
+package com.example.medizap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hospital_Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_signup);
+    }
+}
